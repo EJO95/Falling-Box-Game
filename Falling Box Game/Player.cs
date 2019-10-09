e810ea61df5d4eaccf0c6558fae7cdc55a02866a
arquinn6 <arquinn6@gmail.com>
2019-10-09 14:14:10 -0600

    player update

--- Falling Box Game/Player.cs
+++ Falling Box Game/Player.cs
@@ -10,12 +10,16 @@ namespace Falling_Box_Game
         public bool IsAlive { get; set; }
         public bool IsAirborne { get; set; }
         public bool IsOnSideOfBox { get; set; }
+        public int PlayerPositionX { get; set; }
+        public int PlayerPositionY { get; set; }
         public Player(int sizeOfCharacter)
         {
             Size = sizeOfCharacter;
             IsAlive = true;
             IsAirborne = false;
             IsOnSideOfBox = false;
+            PlayerPositionX = 0;
+            PlayerPositionY = 0;
         }
     }
 }
