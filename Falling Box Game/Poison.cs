6dbb06a858ee7c5fa2cfc33d4737daecd7bb1029
Kate Griffin <katherinegriffinkatherine@gmail.com>
2019-10-09 14:33:54 -0600

    poison class

---
+++ Falling Box Game/Poison.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Falling_Box_Game
+{ 
+
+    class Poison : PoisInterface
+    {
+        public Position Position 
+        { 
+            get => Position;
+            set => new Position(); 
+        }
+
+        public void Grow(double growRate)
+        {
+            Position.setPoint(Position.X + growRate, Position.Y + growRate);
+        }
+
+        public void PlacePoision(double x, double y)
+        {
+            Position.setPoint(x, y);
+        }
+
+        public bool PlayerAndPoison(Player player)
+        {
+            return player.PlayerPositionX <= Position.X && 
+                (player.PlayerPositionY + (player.Size / 2) <= player.PlayerPositionY);
+        }
+    }
+}
