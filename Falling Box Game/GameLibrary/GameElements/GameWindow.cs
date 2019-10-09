4dce47eee756a1127bee73bd53d7e727821222ca
Franklin Wisseh <31441092+FrankDozen@users.noreply.github.com>
2019-10-09 13:52:45 -0600

    GameLibrary added

---
+++ Falling Box Game/GameLibrary/GameElements/GameWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLibrary.GameElements
+{
+    class GameWindow
+    {
+        int WindowHeightTop {get; set;}
+        int WindowHeightBottom {get; set;}
+        //Add methods to set a view window 100px in height with player location at center
+        public void  trackPlayerHeight(IBoardObject player)
+        {
+            WindowHeightTop = player.getY;
+            WindowHeightBottom = width;
+        }
+    }
+}
