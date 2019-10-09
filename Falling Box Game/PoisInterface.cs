f8ed8d92613b900e2c150bda4aef17f7ded045f7
Kate Griffin <katherinegriffinkatherine@gmail.com>
2019-10-09 13:19:07 -0600

    Added Poison Interface and Position Class

---
+++ Falling Box Game/PoisInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Falling_Box_Game
+{
+    interface PoisInterface
+    {
+        Position Position
+        { get; set; }
+
+        void PlacePoision(double x, double y);
+
+        Boolean PlayerAndPoison(Player player);
+
+        void Grow(double growRate);
+
+    }
+}
