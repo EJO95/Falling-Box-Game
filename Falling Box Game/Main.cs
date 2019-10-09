34efbb7ac36010478b181fd7e8404837b0af3c6c
arquinn6 <arquinn6@gmail.com>
2019-10-09 13:32:52 -0600

    Initial Main

---
+++ Falling Box Game/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Falling_Box_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PLAYER_SIZE = 22;
+            const int BOARD_WIDTH = 100;
+            const int BOARD_HEIGHT = 600;
+            const int START_INTERVAL = 500;
+
+            int delayTimer = START_INTERVAL;
+
+            List<Box> boxGroup = new List<Box>();
+
+            Player player = new Player(PLAYER_SIZE);
+            //GameBoard gameBoard = new GameBoard(BOARD_WIDTH, BOARD_HEIGHT);
+            while(player.isAlive)
+            {
+                boxGroup.Add(new Box());
+                //spawn new box
+                //handle checking
+                Thread.Sleep(delayTimer);
+            }
+        }
+    }
+}
